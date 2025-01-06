30eb324cd15800284d8016c9c3b7071976b71801
WHO AM I ? <114454845+yashYT03@users.noreply.github.com>
2025-01-06 12:38:41 +0530

    Add files via upload

---
+++ React/app/src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+<>
+<Navbar />
+</>
+  );
+}
+
+export default App;
+
+
---
+++ React/app/src/module.mjs
@@ -0,0 +1,4 @@
+const a = "alone";
+const q = "anonymous";
+
+export default a;
